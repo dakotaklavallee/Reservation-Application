274ec6be8782467b38ac9e0f3c6232e213a3ef0b
Dakota Lavallee <dakota.k.lavallee@gmail.com>
2022-06-01 09:55:42 -0500

    US-07 DONE

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -27,9 +27,19 @@ function updateStatus(reservation) {
     .then((records) => records[0]);
 }
 
+function search(mobile_number) {
+  return knex("reservations")
+    .whereRaw(
+      "translate(mobile_number, '() -', '') like ?",
+      `%${mobile_number.replace(/\D/g, "")}%`
+    )
+    .orderBy("reservation_date");
+}
+
 module.exports = {
   list,
   read,
   create,
   updateStatus,
+  search,
 };
